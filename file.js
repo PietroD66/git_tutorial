837a0cabf9f61b3afac4af5a0f685e5ff1e32f68
Pietro Diritti <pdiritti@themis.it>
2021-06-25 18:29:40 +0200

    boh

---
+++ file.js
@@ -0,0 +1,8 @@
+var button = document.querySelector("button");
+var h1 = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+    
+    h1.classList.toggle("h1colora");
+    
+})
